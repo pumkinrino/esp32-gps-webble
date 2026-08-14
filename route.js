c752d24ec8ef85b7327a0416ec4c52f8b772a12c
pumkinrino <90735247+pumkinrino@users.noreply.github.com>
2026-08-14 20:05:10 +0700

    Update and rename index.html to route.js

---
+++ route.js
@@ -0,0 +1,25 @@
+export default async function handler(req, res) {
+    const { lat, lon, dlat, dlon } = req.query;
+    const apiKey = "gdk_live_R8lMzHJLuGe_dGk2mWKZ7NuI";
+    
+    try {
+        const url = `https://api.gogoduk.com/v1/directions?origin=${lat},${lon}&destination=${dlat},${dlon}`;
+        const response = await fetch(url, {
+            headers: { "X-API-Key": apiKey }
+        });
+        const data = await response.json();
+        
+        let coords = [];
+        if (data.routes && data.routes[0]) {
+            const steps = data.routes[0].legs[0].steps;
+            for (let step of steps) {
+                coords.push(`${step.start_location.lat},${step.start_location.lng}`);
+            }
+            coords.push(`${dlat},${dlon}`);
+        }
+        
+        res.status(200).send(coords.slice(0, 30).join('|'));
+    } catch (error) {
+        res.status(500).send("ERROR");
+    }
+}
